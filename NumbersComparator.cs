c453ddfe76e014b08a56e2ac3895621ddec28a04
danielparz <dparzynski@gmail.com>
2020-09-28 14:15:05 +0200

    Add project files.

---
+++ NumbersComparator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class NumbersComparator : IComparer<uint>
+    {
+        public int Compare(uint x, uint y)
+        {
+            string strx = x.ToString();
+            string stry = y.ToString();
+
+            int shorter = (strx.Length < stry.Length) ? strx.Length : stry.Length;
+
+            for (int i = 0; i < shorter; i++)
+            {
+                if (strx[i].CompareTo(stry[i]) != 0)
+                    return strx[i].CompareTo(stry[i]);
+            }
+            return 0;
+        }       
+    }
+}
